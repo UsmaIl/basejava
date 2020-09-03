42b5f1088d0ef153b63820be4d700177ad03f17b
IlyaUsmankin <usmankinIlya99@gmail.com>
2020-09-03 19:03:24 +0300

    add implementation of save-method

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -13,7 +13,11 @@ public class ArrayStorage {
     }
 
     void save(Resume r) {
-        storage[]
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
+                storage[i] = r;
+            }
+        }
     }
 
     Resume get(String uuid) {
