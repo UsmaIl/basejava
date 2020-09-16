800cf175b776ad9ea435d1b1cde5f9dbb63cbace
IlyaUsmankin <usmankinIlya99@gmail.com>
2020-09-16 12:22:26 +0300

    create new directory and redistribution files

--- src/com/dofler/webapp/storage/ArrayStorage.java
+++ src/com/dofler/webapp/storage/ArrayStorage.java
@@ -1,30 +1,34 @@
+package com.dofler.webapp.storage;
+
+import com.dofler.webapp.model.Resume;
+
 import java.util.Objects;
 
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    private final Resume[] storage = new Resume[10000];
     private int iterator = 0;
 
-    void clear() {
+    public void clear() {
         for (int i = 0; i < iterator; i++) {
             storage[i] = null;
         }
         iterator = 0;
     }
 
-    void save(Resume resume) {
+    public void save(Resume resume) {
         if (resume == null) {
-            System.out.println("В метод save класса ArrayStorage передан null!!!");
+            System.out.println("В метод save класса com.dofler.webapp.storage.ArrayStorage передан null!!!");
             return;
         }
         storage[iterator++] = resume;
     }
 
-    Resume get(String uuid) {
+    public Resume get(String uuid) {
         if (uuid == null) {
-            System.out.println("В метод get класса ArrayStorage передан null!!!");
+            System.out.println("В метод get класса com.dofler.webapp.storage.ArrayStorage передан null!!!");
         }
 
         for (int i = 0; i < iterator; i++) {
@@ -36,9 +40,9 @@ public class ArrayStorage {
         return null;
     }
 
-    void delete(String uuid) {
+    public void delete(String uuid) {
         if (uuid == null) {
-            System.out.println("В метод delete класса ArrayStorage передан null!!!");
+            System.out.println("В метод delete класса com.dofler.webapp.storage.ArrayStorage передан null!!!");
             return;
         }
 
@@ -54,7 +58,7 @@ public class ArrayStorage {
     /**
      * @return array, contains only Resumes in storage (without null)
      */
-    Resume[] getAll() {
+    public Resume[] getAll() {
         Resume[] resumes = new Resume[iterator];
 
         for (int i = 0; i < iterator; i++) {
@@ -64,7 +68,7 @@ public class ArrayStorage {
         return resumes;
     }
 
-    int size() {
+    public int size() {
         return iterator;
     }
 }
