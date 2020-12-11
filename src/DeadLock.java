f984396c0f021e91ba7dac0a34014c0232be7552
IlyaUsmankin <usmankinIlya99@gmail.com>
2020-12-11 13:48:56 +0300

    refactoring for HW11

--- src/DeadLock.java
+++ src/DeadLock.java
@@ -3,37 +3,29 @@ public class DeadLock {
     public final static Object secondLock = new Object();
 
     public static void main(String[] args) {
-        new Thread(() -> {
-            synchronized (fistLock) {
-                System.out.println("Thread №1: Holding first lock.");
-
-                try {
-                    Thread.sleep(10);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                System.out.println("Thread №1: Waiting for second lock.");
-
-                synchronized (secondLock) {
-                    System.out.println("Thread №1: Holding both.");
-                }
-            }
-        }).start();
-        new Thread(() -> {
-            synchronized (secondLock) {
-                System.out.println("Thread №2: Holding second lock.");
-
-                try {
-                    Thread.sleep(10);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                System.out.println("Thread №2: Waiting for first lock.");
-
-                synchronized (fistLock) {
-                    System.out.println("Thread №2: Holding both.");
-                }
-            }
-        }).start();
+        new Thread(() -> testForDeadLock(fistLock, secondLock)).start();
+        new Thread(() -> testForDeadLock(secondLock, fistLock)).start();
     }
+
+    public static void testForDeadLock(Object fistLock, Object secondLock) {
+        synchronized (fistLock) {
+            System.out.println(getCurrentThread() + ": Holding second lock.");
+
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(getCurrentThread() + ": Waiting for first lock.");
+
+            synchronized (secondLock) {
+                System.out.println(getCurrentThread() + ": Holding both.");
+            }
+        }
+    }
+
+    private static String getCurrentThread() {
+        return Thread.currentThread().toString();
+    }
+
 }
