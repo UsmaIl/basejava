e1dfbb59c94ccf3a7b842633502960b9a3961edd
IlyaUsmankin <usmankinIlya99@gmail.com>
2020-12-22 20:03:01 +0300

    rename class and change implementation of minValue

---
+++ src/MainStreamAPI.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class MainStreamAPI {
+    public static void main(String[] args) {
+        int[] testArray = new int[]{2, 3, 4, 4, 5, 5, 1, 1, 2, 0, 0};
+        System.out.println(minValue(testArray));
+        System.out.println(oddOrEven(Arrays.asList(2, 3, 4, 5, 6, 7, 13)));
+    }
+
+    public static int minValue(int[] value) {
+        return Arrays.stream(value).distinct().sorted()
+                .reduce(0, (x, y) -> (int) Math.pow(10, (int) (Math.log10(y) + 1)) * x + y);
+    }
+
+    public static List<Integer> oddOrEven(List<Integer> integers) {
+        Map<Boolean, List<Integer>> map = integers.stream()
+                .collect(Collectors.partitioningBy(x -> x % 2 == 0, Collectors.toList()));
+        return map.get(map.get(false).size() % 2 != 0);
+    }
+}
