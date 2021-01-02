6d5b13ba2dec3884b6fdeec2365abfb130aa4092
IlyaUsmankin <usmankinIlya99@gmail.com>
2021-01-02 16:46:56 +0300

    create config-file for retrieving specific props and initializing SQLStorage

---
+++ test/com/dofler/webapp/Config.java
@@ -0,0 +1,42 @@
+package com.dofler.webapp;
+
+import com.dofler.webapp.storage.SQLStorage;
+import com.dofler.webapp.storage.Storage;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.util.Properties;
+
+public class Config {
+    private static final File PROPS = new File("config\\resumes.properties");
+    private static final Config INSTANCE = new Config();
+
+    private final File storageDir;
+    private final Storage storage;
+
+    public static Config getInstance() {
+        return INSTANCE;
+    }
+
+    private Config() {
+        try (InputStream is = new FileInputStream(PROPS)) {
+            Properties props = new Properties();
+            props.load(is);
+            storageDir = new File(props.getProperty("storage.dir"));
+            storage = new SQLStorage(props.getProperty("db.url"), props.getProperty("db.user"), props.getProperty("db.password"));
+        } catch (IOException e) {
+            throw new IllegalStateException("Invalid config file " + PROPS.getAbsolutePath());
+        }
+    }
+
+    public File getStorageDir() {
+        return storageDir;
+    }
+
+    public Storage getStorage() {
+        return storage;
+    }
+}
