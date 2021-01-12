b1c2b1ec3dfa6d076b15cccf952b2bf87d3f13a3
IlyaUsmankin <usmankinIlya99@gmail.com>
2021-01-12 15:05:01 +0300

    add annotation @XmlRootElemet to this class

--- src/com/dofler/webapp/model/AbstractSection.java
+++ src/com/dofler/webapp/model/AbstractSection.java
@@ -2,8 +2,10 @@ package com.dofler.webapp.model;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
 import java.io.Serializable;
 
+@XmlRootElement
 @XmlAccessorType(XmlAccessType.FIELD)
 public abstract class AbstractSection implements Serializable {
     protected static final long serialVersionUID = 1L;
