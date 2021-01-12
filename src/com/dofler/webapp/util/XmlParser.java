bebbfb19b9442dfc989fdcafd017626995688ba2
IlyaUsmankin <usmankinIlya99@gmail.com>
2021-01-12 15:01:43 +0300

    add auiliary methods for marshall/unmarshall

--- src/com/dofler/webapp/util/XmlParser.java
+++ src/com/dofler/webapp/util/XmlParser.java
@@ -1,16 +1,22 @@
 package com.dofler.webapp.util;
 
 
+import com.dofler.webapp.model.AbstractSection;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 import java.io.Reader;
+import java.io.StringWriter;
 import java.io.Writer;
 
 public class XmlParser {
     private final Marshaller marshaller;
     private final Unmarshaller unmarshaller;
+    private final StringWriter sw;
 
     public XmlParser(Class... classesToBeBound) {
         try {
@@ -22,9 +28,32 @@ public class XmlParser {
             //marshaller.setProperty(Marshaller.JAXB_FRAGMENT, true);
 
             unmarshaller = ctx.createUnmarshaller();
+            sw = new StringWriter();
         } catch (JAXBException e) {
             throw new IllegalStateException(e);
         }
+    }
+
+
+    public void marshall(Object instance, Writer writer) {
+        try {
+            marshaller.marshal(instance, writer);
+        } catch (JAXBException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    public <T> T unmarshall(String section) throws JAXBException {
+        return (T) unmarshaller.unmarshal(marshaller.getNode(section));
+    }
+
+    public String marshall(Object instance) {
+        try {
+            marshaller.marshal(instance, sw);
+        } catch (JAXBException e) {
+            throw new IllegalStateException(e);
+        }
+        return sw.toString();
     }
 
     public <T> T unmarshall(Reader reader) {
@@ -35,11 +64,5 @@ public class XmlParser {
         }
     }
 
-    public void marshall(Object instance, Writer writer) {
-        try {
-            marshaller.marshal(instance, writer);
-        } catch (JAXBException e) {
-            throw new IllegalStateException(e);
-        }
-    }
+
 }
